7ccd1f85395e5283e9510d0d4b5d291570c9eeae
johnmccants002 <johnmccants002@gmail.com>
2022-07-30 01:10:56 -0700

    Initial Commit

---
+++ realfriends/realfriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  realfriendsApp.swift
+//  realfriends
+//
+//  Created by John McCants on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct realfriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
